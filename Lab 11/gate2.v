3ef32b762ce8bb3388a06080215ed0fcf900feb5
Bryce Monaco <Bryce@BrycBook-Pro.local>
2017-04-26 16:26:10 -0700

    Added comments to Lab 11

--- Lab 11/gate2.v
+++ Lab 11/gate2.v
@@ -1,8 +1,12 @@
+//Bryce Monaco, Alex Kastanek
+//CPE 301 - Lab 11 - Question 2
+//Version 1.0
+
 module gate2 (in_a, in_b, out);
 
 input wire in_a;	// declare input variable in_a
-input wire in_b;
-output wire [5:0] out;
+input wire in_b;	// declare input variable in_b
+output wire [5:0] out;	// output variables, mapped to LEDs above switches
 
 assign out[5] = in_a & in_b;
 assign out[4] = ~(in_a & in_b);
@@ -12,3 +16,6 @@ assign out[1] = in_a ^ in_b;
 assign out[0] = ~(in_a ^ in_b);
 
 endmodule
+
+//Note: Originally out[1]’s assignment didn’t work, not because of the
+//..code but because the tutorial maps it to the incorrect output
--- Lab 11/number3.v
+++ Lab 11/number3.v
@@ -1,16 +1,26 @@
+//Bryce Monaco, Alex Kastanek
+//CPE 301 - Lab 11 - Question 3
+//Version 1.0
+
 module number3 (in_a, in_b, in_en, out, segOut);
 
-input wire in_a;	// declare input variable in_a
+//Input Variables
+input wire in_a;	
 input wire in_b;
 input wire in_en;
-output wire [4:0] out;
-output wire [6:0] segOut;
 
+//Output Variables
+output wire [4:0] out;		//Set of LEDs above switches (2to4 debug)
+output wire [6:0] segOut;	//7 set display HEX0
+
+//2 to 4 Decoder, outputs to LEDs for debug
 assign out[3] = in_a & in_b & in_en;
 assign out[2] = ~in_a & in_b & in_en;
 assign out[1] = in_a & ~in_b & in_en;
 assign out[0] = ~in_a & ~in_b & in_en;
 
+//Uses the decoder output to decide which segments to enable
+//..note segments are active low, so writing a 1 turns them off
 assign segOut[6] = ~in_en | out[0];
 assign segOut[5] = out[0] | out[1] | out[2];
 assign segOut[4] = out[0] | out[2] | out[3];
